0a25418185ea52f285c9e396edb4dd4d02ed7dd3
Raji <raji.waktole@uau.edu>
2026-04-15 07:47:53 -0500

    Initial commit

---
+++ app/page.ts
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <main style={{ padding: "2rem", fontFamily: "sans-serif" }}>
+      <h1>Next.js + MongoDB + Docker Compose</h1>
+      <p>App is running inside Docker.</p>
+      <p>Try calling <code>/api/test</code> to hit MongoDB.</p>
+    </main>
+  );
+}
---
+++ lib/mongodb.ts
@@ -0,0 +1,25 @@
+import { MongoClient } from "mongodb";
+
+if (!process.env.MONGODB_URI) {
+  throw new Error("MONGODB_URI is not defined");
+}
+
+const uri = process.env.MONGODB_URI;
+const options = {};
+
+let client: MongoClient;
+let clientPromise: Promise<MongoClient>;
+
+declare global {
+  // eslint-disable-next-line no-var
+  var _mongoClientPromise: Promise<MongoClient> | undefined;
+}
+
+if (!global._mongoClientPromise) {
+  client = new MongoClient(uri, options);
+  global._mongoClientPromise = client.connect();
+}
+
+clientPromise = global._mongoClientPromise;
+
+export default clientPromise;
